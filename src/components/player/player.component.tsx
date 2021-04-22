b34d51e4a8419abccce973de7528586d3be497d3
Paul-Mad <paulin.mad@hotmail.com>
2021-04-22 19:59:58 +0100

    player slider and buttons styles edited when there is an episode  being played

--- src/components/player/player.component.tsx
+++ src/components/player/player.component.tsx
@@ -1,5 +1,9 @@
 import { useContext } from "react";
+import Image from "next/image";
 import { PlayerContext } from "../../contexts/playersContext";
+
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 import styles from "./styles.module.scss";
 
 const Player = () => {
@@ -13,35 +17,61 @@ const Player = () => {
     <div className={styles.playerContainer}>
       <header>
         <img src="/playing.svg" alt="Playing Now" />
-        <strong>Playing Now {episode?.title}</strong>
+        <strong>Playing Now </strong>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Select a podcast to listen</strong>
-      </div>
-      <footer className={styles.empty}>
+      {episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Select a podcast to listen</strong>
+        </div>
+      )}
+
+      <footer className={!episode ? styles.empty : ""}>
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: "#04d361" }}
+                railStyle={{ backgroundColor: "#9f75ff" }}
+                handleStyle={{ borderColor: "#04d361", borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
 
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="shuffle" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt=" Play previous" />
           </button>
-          <button type="button" className={styles.playButton}>
+          <button
+            type="button"
+            className={styles.playButton}
+            disabled={!episode}
+          >
             <img src="/play.svg" alt="Play" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Play next" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repeat" />
           </button>
         </div>
